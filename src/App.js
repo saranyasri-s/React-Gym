7e27f178c52ef831fcacb19cec56496cdb0447d0
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2023-12-30 14:37:24 +0530

    added navbar footer and pages files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import classes from "./App.module.css";
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Home from "./Pages/Home";
+import ExerciseDetails from "./Pages/ExerciseDetails";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
-  return <div className={classes.App}>hi</div>;
+  return (
+    <div className={classes.App}>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route
+          path="/exercise/:id"
+          element={<ExerciseDetails></ExerciseDetails>}
+        ></Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/ExerciseDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from "./ExerciseDetails.module.css"
+function ExerciseDetails() {
+  return (
+    <div>ExerciseDetails</div>
+  )
+}
+
+export default ExerciseDetails
