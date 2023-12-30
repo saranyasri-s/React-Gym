682ff11ce6b2d6c53b4112cd846e1fdb3401f5f5
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2023-12-30 21:15:07 +0530

    added search exercises input box

--- src/Components/HeroBanner.js
+++ src/Components/HeroBanner.js
@@ -14,18 +14,32 @@ function HeroBanner() {
       </Typography>
       <Typography
         fontWeight={700}
+        mb="23px"
+        mt="30px"
         sx={{ fontSize: { lg: "44px", xs: "40px" } }}
       >
         Sweat, Smile <br />
         and Repeat
       </Typography>
-      <Typography fontSize="22px" lineHeight="35px" mb="30px">
+      <Typography mb={4} fontSize="22px" lineHeight="35px">
         Check out the most effective exercises
       </Typography>
-      <Button variant="contained" color="error" href="#exercises">
+      <Button
+        sx={{ backgroundColor: "#ff2625", padding: "10px" }}
+        variant="contained"
+        color="error"
+        href="#exercises"
+      >
         Explore Exercises
       </Button>
-      <Typography fontWeight={600} color="ff2625" sx={{opacity:0.1}}>Exercises</Typography>
+      <Typography
+        fontWeight={600}
+        color="#ff2625"
+        fontSize="200px"
+        sx={{ opacity: 0.1, display: { lg: "block", xs: "none" } }}
+      >
+        Exercise
+      </Typography>
       <img src={HeroBannerImage} alt="banner" className="hero-banner-img"></img>
     </Box>
   );
--- src/Components/SearchExercises.js
+++ src/Components/SearchExercises.js
@@ -1,9 +1,51 @@
-import React from 'react'
-import classes from "./SearchExercises.module.css"
+import React, { useState, useEffect } from "react";
+
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 function SearchExercises() {
   return (
-    <div>SearchExercises</div>
-  )
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises you
+        <br /> should know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          height="76px"
+          value=""
+          onChange={(e) => {}}
+          placeholder="Search Exercises"
+          type="text"
+          sx={{
+            input: { fontWeight: "700", border: "none", borderRadius: "4px" },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "white",
+            borderRadius: "40px",
+          }}
+        ></TextField>
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            right: "5",
+          }}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
 }
 
-export default SearchExercises
+export default SearchExercises;
