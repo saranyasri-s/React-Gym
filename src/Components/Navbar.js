3cd9d4ff5d2e95919df02054428cb2308097a4a0
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2023-12-30 15:31:44 +0530

    added navbar using material ui

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,48 @@
-import React from 'react'
-import classes from "./Navbar.module.css"
+import React from "react";
+import classes from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+import Logo from "../assets/images/Logo.png";
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <Stack
+        direction="row"
+        justifyContent="space-around"
+        sx={{
+          gap: { sm: "124px", xs: "40px" },
+          mt: { sm: "32px", xs: "20px" },
+        }}
+      >
+        <Link to="/">
+          <img
+            src={Logo}
+            alt="logo"
+            style={{ width: "48px", height: "48px", margin: "0 20px" }}
+          ></img>
+        </Link>
+
+        <Stack direction="row" gap="40px" fontSize="24px" alignItems="center">
+          <Link
+            to="/"
+            style={{
+              textDecoration: "none",
+              color: "#3A1212",
+              borderBottom: "3px solid #FF2625",
+            }}
+          >
+            Home
+          </Link>
+          <a
+            href="#exercises"
+            style={{ textDecoration: "none", color: "#3A1212" }}
+          >
+            Exercises{" "}
+          </a>
+        </Stack>
+      </Stack>{" "}
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
