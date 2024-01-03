b2b503c1d5678e38ef56b29769778032162ab280
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2024-01-03 09:29:05 +0530

    added similar equipment exercises component

--- src/Components/SimilarExercises.js
+++ src/Components/SimilarExercises.js
@@ -5,14 +5,29 @@ import HorizontalScrollBar from "../Components/HorizontalScrollBar";
 function SimilarExercises({ targetMuscleExercises, equipmentExercises }) {
   return (
     <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
-      <Typography variant="h3">
+      <Typography variant="h3" mb={5}>
         {console.log(targetMuscleExercises)}
         Exercises that target the same muscle group
       </Typography>
-      <Stack direction="row" sx={{ p: "2", position: "relative", m: "2rem" }}>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
         {targetMuscleExercises.length ? (
           <HorizontalScrollBar
             data={targetMuscleExercises}
+            isBodyParts={false}
+          ></HorizontalScrollBar>
+        ) : (
+          // <p>tduytduj</p>
+          <Loader></Loader>
+        )}
+      </Stack>
+      <Typography variant="h3" mt={10} mb={5}>
+        {console.log(targetMuscleExercises)}
+        Exercises that uses the same equipment
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {equipmentExercises.length ? (
+          <HorizontalScrollBar
+            data={equipmentExercises}
             isBodyParts={false}
           ></HorizontalScrollBar>
         ) : (
